f6c2c0498897d72cdc184589d12ae76272b2bfbd
henrique-monteiro <henrique-monteiro@hotmail.com>
2021-07-23 07:31:47 -0300

    initial: initial project with mysql configuration

---
+++ src/main/java/com/googlebooksapi/googlebooksapi/GoogleBooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.googlebooksapi.googlebooksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleBooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleBooksApiApplication.class, args);
+	}
+
+}
